e2b464c3a2b9d2195fa0120073ea5eaa52e1d3e3
Amanpathak1 <155675919+Amanpathak1@users.noreply.github.com>
2024-04-17 06:56:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 Var = os.environ.get
 
-APP_ID= int(Var("APP_ID"))
-API_HASH = Var("API_HASH")
-BOT_TOKEN = Var("BOT_TOKEN")
-LOG_CHANNEL = int(Var("LOG_CHANNEL"))
+APP_ID= int(Var("22806687"))
+API_HASH = Var("19bd4524c11fc958d2cc4b390c4dac15")
+BOT_TOKEN = Var("6753312592:AAFSSEoSTQN7Uma4EmTzBfulwVELVXegXhM")
+LOG_CHANNEL = int(Var("-1002139487358"))
